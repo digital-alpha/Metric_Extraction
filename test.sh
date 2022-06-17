df2ffc18cddc298bdc978df6d760823f4fecdbfd
Ayush Tiwari <ayushtiwari26402@gmail.com>
2022-06-17 21:31:49 +0530

    folder structure

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 start_time=`date +%s`
-cd sec-scrapper &&
+cd sec_scrapper &&
 python3 edgar_crawler.py &&
 python3 extract_items_10k.py &&
-cd ../NLP_Pipeline &&
-python3 nlp_pipeline.py &&
+cd ../nlp_pipeline &&
+python3 main.py &&
 cd ..
 end_time=`date +%s`
 echo Execution Time was `expr $end_time - $start_time` s.
